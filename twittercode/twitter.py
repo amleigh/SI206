30cffab6d825f2c08398187ddddc07901d4a044c
Amanda Leigh <amandaleighmcleod@comcast.net>
2016-11-07 15:17:27 -0500

    updates twitter code

--- twittercode/twitter.py
+++ twittercode/twitter.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Unique code from Twitter
-access_token = "YOUR CODE HERE"
-access_token_secret = "YOUR CODE HERE"
-consumer_key = "YOUR CODE HERE"
-consumer_secret = "YOUR CODE HERE"
+access_token = "55384204-2U7JpuQPBXwn1SLksxFm1xIOXlWXHFa2myBjyZ0FC"
+access_token_secret = "6LngNVVSaNNdbGx9470fQwmO7se7sCtEO6AFBZ6t8DOYX"
+consumer_key = "8ESXWYcgBc2J4nnZEFdHqCHwR"
+consumer_secret = "UKUN8ScdyN16bkLrU5QdYdGnBsdES81HLud3OrQUYHIVN47XEt"
 
 # Boilerplate code here
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
